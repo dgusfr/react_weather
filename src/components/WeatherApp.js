0fd2f8c64dbd31c338d0b37d4932a8748e21a832
dgusfr <diegogustavo.fr@gmail.com>
2024-09-05 09:23:42 -0300

    feat(WeatherApp): adição de estado para armazenar a cidade digitada

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function WeatherApp() {
+  const [city, setCity] = useState("");
+
   return (
     <div>
       <header>
         <h1>Previsão do Tempo</h1>
-        <input type="text" placeholder="Digite o nome da cidade" />
+        <input
+          type="text"
+          placeholder="Digite o nome da cidade"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
       </header>
     </div>
   );
