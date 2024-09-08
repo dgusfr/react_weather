16192f81b268f34475fc6457172e3ab71ccc2097
dgusfr <diegogustavo.fr@gmail.com>
2024-09-08 13:02:48 -0300

    feat(WeatherApp): exibição do país junto com a cidade

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -77,6 +77,21 @@ function WeatherApp() {
       <button onClick={fetchWeather} disabled={loading}>
         {loading ? "Buscando..." : "Buscar"}
       </button>
+
+      {weatherData && (
+        <div>
+          <h2>
+            {weatherData.name}, {weatherData.sys.country}
+          </h2>
+          <img
+            src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+            alt={weatherData.weather[0].description}
+          />
+          <p>Temperatura: {weatherData.main.temp}°C</p>
+          <p>Umidade: {weatherData.main.humidity}%</p>
+          <p>Condição: {weatherData.weather[0].description}</p>
+        </div>
+      )}
     </div>
   );
 }
