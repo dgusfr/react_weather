e1fe9e3425cf22c7c3041a5cf4ab5b4260b59b66
dgusfr <diegogustavo.fr@gmail.com>
2024-09-13 07:38:03 -0300

    feat(weatherService): criação de função para buscar previsão horária detalhada

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,14 +1,15 @@
-export const getForecastByCity = async (city) => {
+export const getHourlyForecastByCity = async (city) => {
   try {
     const response = await axios.get(`${API_BASE_URL}/forecast`, {
       params: {
         q: city,
         appid: API_KEY,
         units: "metric",
+        cnt: 8, // Número de horas para a previsão
       },
     });
     return response.data;
   } catch (error) {
-    throw new Error("Erro ao buscar dados de previsão estendida.");
+    throw new Error("Erro ao buscar previsão horária.");
   }
 };
