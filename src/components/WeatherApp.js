54948f3ae906428d91815a0121fdcd0358589d19
dgusfr <diegogustavo.fr@gmail.com>
2024-09-15 15:22:48 -0300

    feat(WeatherApp): adição de ícones climáticos ao cabeçalho da previsão

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -82,6 +82,19 @@ function WeatherApp() {
 
       {/* Exibição da previsão horária */}
       {hourlyForecast && <HourlyForecast hourlyData={hourlyForecast} />}
+
+      {weatherData && (
+        <div>
+          <h2>
+            {weatherData.name}, {weatherData.sys.country}
+          </h2>
+          <img
+            src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+            alt={weatherData.weather[0].description}
+          />
+          <p>Temperatura: {weatherData.main.temp}°C</p>
+        </div>
+      )}
     </div>
   );
 }
