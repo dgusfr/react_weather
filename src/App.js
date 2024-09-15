2f5d9ae719c4aa30e757bbb4b31c0048fc2870f0
dgusfr <diegogustavo.fr@gmail.com>
2024-09-15 15:24:08 -0300

    feat(App): exibição do componente Footer no App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WeatherApp from "./components/WeatherApp";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <WeatherApp />
+      <Footer />
     </div>
   );
 }
