7117f5078571c3cb815c386246d57281c213720d
dgusfr <diegogustavo.fr@gmail.com>
2024-09-15 15:23:45 -0300

    feat(Footer): criação de um footer simples com informações do projeto

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Previsão do Tempo - Desenvolvido com React</p>
+    </footer>
+  );
+}
+
+export default Footer;
