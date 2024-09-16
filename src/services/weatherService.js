2c72625161bd29b9e31646508d6cbdc55488790c
dgusfr <diegogustavo.fr@gmail.com>
2024-09-16 09:58:27 -0300

    refactor(weatherService): refatoração de lógica para melhorar reutilização de código

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,17 +1,13 @@
-const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
-const API_BASE_URL = "https://api.openweathermap.org/data/2.5";
-
-export const getWeatherByCity = async (city) => {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/weather`, {
-      params: {
-        q: city,
-        appid: API_KEY,
-        units: "metric",
-      },
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error("Erro ao buscar dados de previsão.");
-  }
+const fetchDataFromAPI = async (endpoint, params = {}) => {
+  const response = await axios.get(`${API_BASE_URL}/${endpoint}`, {
+    params: {
+      ...params,
+      appid: API_KEY,
+      units: "metric",
+    },
+  });
+  return response.data;
 };
+
+export const getWeatherByCity = (city) =>
+  fetchDataFromAPI("weather", { q: city });
