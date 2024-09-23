392fa1a9f9e7eabff124911652f5f3a53ccea4ac
dgusfr <diegogustavo.fr@gmail.com>
2024-09-23 10:06:40 -0300

    feat(button): align burttton

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-// URL base da API e chave de API a partir das variáveis de ambiente
 const API_BASE_URL = "https://api.openweathermap.org/data/2.5";
-const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY; // Chave retirada do .env
+const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
 
-// Função para buscar o clima atual pela cidade
 export const getWeatherByCity = async (city) => {
   try {
     const response = await axios.get(`${API_BASE_URL}/weather`, {
@@ -21,7 +19,6 @@ export const getWeatherByCity = async (city) => {
   }
 };
 
-// Função para buscar a previsão de 5 dias pela cidade
 export const getForecastByCity = async (city) => {
   try {
     const response = await axios.get(`${API_BASE_URL}/forecast`, {
